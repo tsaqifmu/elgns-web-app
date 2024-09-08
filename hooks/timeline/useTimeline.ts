1334006bd0395ca66f513620e42625fe1cfe10b7
tsaqifmu <mushoddaqt@gmail.com>
2024-09-08 21:10:12 +0700

    feat: make useParams for month filter and fixing colors for each cusomter data

--- hooks/timeline/useTimeline.ts
+++ hooks/timeline/useTimeline.ts
@@ -1,13 +1,18 @@
 import { apiRequest, HttpMethod } from "@/lib/apiRequest";
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
 
 export const uesFetchTimelineData = () => {
+  const searchParams = useSearchParams();
+  const month = searchParams.get("month");
+
   return useQuery({
-    queryKey: ["timeline"],
+    queryKey: ["timeline", month],
     queryFn: async () => {
       const { data } = await apiRequest({
         path: "/production/calendar",
         method: HttpMethod.GET,
+        params: { month: Number(month), year: "2024" },
       });
       return data;
     },
