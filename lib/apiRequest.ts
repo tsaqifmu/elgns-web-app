59c6a519731dd84a02d3befc073e7af49553254a
Verry <verryrwibawa@gmail.com>
2024-09-16 09:55:37 +0700

    fix: set tasks state on monitoring data update

--- lib/apiRequest.ts
+++ lib/apiRequest.ts
@@ -54,9 +54,9 @@ export const apiRequest = async ({
     (response) => response,
     (error) => {
       console.log(error);
-      // if (error.response?.status === 401) {
-      //   window.location.href = "/login";
-      // }
+      if (error.response?.status === 401) {
+        window.location.href = "/login";
+      }
       return Promise.reject(error);
     },
   );
